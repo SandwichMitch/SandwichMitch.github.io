e4ace7b9899174ee6db4086a0554fede8f2390d9
Ryan Ruiming Li <143842796+SandwichMitch@users.noreply.github.com>
2024-09-23 00:11:44 -0400

    added video projects youtube

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function changeVideo(videoId) {
+    const iframe = document.getElementById('videoPlayer');
+    iframe.src = `https://www.youtube.com/embed/${videoId}`;
+  }
