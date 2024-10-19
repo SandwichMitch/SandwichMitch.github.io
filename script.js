be09f569b82ed6e47d20671af8dd140de997b13f
Ryan Ruiming Li <143842796+SandwichMitch@users.noreply.github.com>
2024-10-18 23:18:58 -0400

    Aesthetics update
    Nearly completed visuals, need to content

--- script.js
+++ script.js
@@ -8,4 +8,32 @@ function toggleMenu() {
 function changeVideo(videoId) {
     const iframe = document.getElementById('videoPlayer');
     iframe.src = `https://www.youtube.com/embed/${videoId}`;
-  }
+}
+
+let lastButtonPressed = document.getElementById('btnVFX');; // Variable to track the last pressed button
+
+function handleButtonClick(button) {
+    // Reset the last button pressed, if there is one
+      if (lastButtonPressed && lastButtonPressed !== button) {
+          lastButtonPressed.className = 'btn btn-color-2'; // Change back to the original class
+     }
+     
+      // Change the class of the currently pressed button
+    button.className = 'btn btn-color-1'; // Change to the new class
+    const projectText = document.getElementById('project-text');
+    switch (button.id) {
+        case 'btnVFX':
+            projectText.textContent = 'This is the VFX and Editing Reel project.';
+            break;
+        case 'btnRushVideo':
+            projectText.textContent = 'This is the Delta Upsilon Rush Video project.';
+            break;
+        case 'btnShortFilm':
+            projectText.textContent = 'This is the Short Film: Buzzing Bad project.';
+            break;
+        default:
+            projectText.textContent = 'This is the original project text.';
+    }
+     // Update the lastButtonPressed variable
+   lastButtonPressed = button;
+}
